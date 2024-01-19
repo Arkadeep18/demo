abbd2388fad332f5f6095fa2b6fc0b1186a99492
Arka <146548597+Arkadeep18@users.noreply.github.com>
2024-01-19 22:04:07 +0530

    Update Prac15.java
    soring 3 numbers in ascending order

--- Prac15.java
+++ Prac15.java
@@ -1,25 +1,15 @@
-import java.util.*;
 
-public class Prac15 {
+public class Cmd2 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {		
+		int a = 4;
+		int b = 90;
+		int c = 9;		//ascending order sorting
 		
-		System.out.println("Subject 1");
-		float subject1 = sc.nextFloat();
-		System.out.println("Subject 2");
-		float subject2 = sc.nextFloat();
-		System.out.println("Subject 3");
-		float subject3 = sc.nextFloat();
-		System.out.println("Subject 4");
-		float subject4 = sc.nextFloat();
-		System.out.println("Subject 5");
-		float subject5 = sc.nextFloat();
-		
-		float cgpa = (subject1 + subject2 + subject3 + subject4 + subject5)/50f;
-		System.out.println(cgpa +" cgpa");
-
-
-	}
+		int firstnum = Math.max(Math.max(a, b),c);
+		int secnum = Math.min(Math.min(a, b), c);
+		int midnum =(a+b+c)-(firstnum+secnum);
+		System.out.println(firstnum+" > "+midnum+" > "+secnum);
+		}
 
 }
